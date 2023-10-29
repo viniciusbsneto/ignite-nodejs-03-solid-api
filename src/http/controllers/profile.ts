57d2c384458b3ffbb45522fe465a1de25a664d13
Vinícius Neto <viniciusbsneto@gmail.com>
2023-10-29 18:45:42 -0300

    feat: apply JWT verification middleware to the profile route

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,26 +1,15 @@
-import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
 import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
 
 export const profile = async (request: FastifyRequest, reply: FastifyReply) => {
-  const registerBodySchema = z.object({
-    userId: z.string(),
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({ id: request.user.sub })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
   })
-
-  const { userId } = registerBodySchema.parse(request.body)
-
-  try {
-    const getUserProfileUseCase = makeGetUserProfileUseCase()
-
-    await getUserProfileUseCase.execute({ id: userId })
-  } catch (error) {
-    if (error instanceof ResourceNotFoundError) {
-      return reply.status(404).send({ message: error.message })
-    }
-
-    throw error
-  }
-
-  return reply.status(200).send()
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { verifyJWT } from './controllers/middlewares/verify-jwt'
 
 export const appRoutes = async (app: FastifyInstance) => {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   // Authenticated routes
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
