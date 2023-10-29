68eccc7e92837d71fbfcbf97c4eabdfa7d91424b
Vinícius Neto <viniciusbsneto@gmail.com>
2023-10-29 18:45:00 -0300

    feat: add JWT sign to the authentication controller

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -17,7 +17,20 @@ export const authenticate = async (
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({
+      token,
+    })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
@@ -25,6 +38,4 @@ export const authenticate = async (
 
     throw error
   }
-
-  return reply.status(200).send()
 }
